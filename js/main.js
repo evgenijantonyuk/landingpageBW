3b88a53c346f21eb9fe0db3cf1ea46e0e697eda2
Evgenij Antonyuk <96491514+evgenijantonyuk@users.noreply.github.com>
2023-06-18 12:59:12 +0700

    111

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // плавная прокрутка
+    $('nav a[href^="#"]').click(function() {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        // активная ссылка
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        $('.menu__mobile .menu').toggle(500);
+        $('.menu__burger').toggleClass('close');
+        return false;
+    });
+//     Мобильное меню
+    $('.menu__burger').click(function () {
+       $('.menu__mobile .menu').toggle(500);
+       $(this).toggleClass('close');
+    });
+});
